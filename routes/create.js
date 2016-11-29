24879a2c1483221d97dd70fa85cce75f2c885f9f
Belem Zhang <belem.zhang@intel.com>
2016-11-29 16:51:19 +0800

    u

--- routes/create.js
+++ routes/create.js
@@ -1,15 +1,95 @@
-let router = require('koa-router')();
+let router = require('koa-router')()
+let fs = require('fs')
+
+
+let getDateTime = function () {
+  let date = new Date()
+  return date.getFullYear() + '' + date.getMonth() + date.getDate() + date.getHours() + date.getMinutes()
+   
+}
+
+let createMD = function (title, time, tags, categories, cover, verify, article) {
+
+  let tagstring = ''
+  if (tags.indexOf(',') > -1) {
+    let tagsplit = tags.split(',')
+    for (let i of tagsplit) {
+      if (i.trim()) {
+        let tstring = '- ' + i.trim() + '\r\n'
+        tagstring += tstring
+      }
+    }
+  } else {
+    tagstring = '- ' + tags + '\r\n'
+  }
+
+  console.log('00000000000' + article)
+
+  let data = 'title:' + title + '\r\n' +
+    'date:' + time + '\r\n' +
+    'tags:\r\n' + tagstring + 'categories:\r\n' +
+    '- ' + categories + '\r\n' + 'cover: ' + cover + '\r\n' +
+    '---' + '\r\n' + article
+
+  // /var/www/anr.io/source/_posts/
+
+  let writerStream = fs.createWriteStream('/home/belem/Desktop/tmp/' + getDateTime() + '.md')
+  writerStream.write(data, 'UTF8')
+  writerStream.end()
+
+  writerStream.on('finish', function () {
+    console.log(title + ': .md file created.')
+  })
+
+  writerStream.on('error', function (err) {
+    console.log(err)
+  })
+}
 
 router.post('/', async function (ctx, next) {
- let title =ctx.request.body.title || 0;
- let time =ctx.request.body.time || 0;
- ctx.body = "you post data:"+JSON.stringify({title:title});
- ctx.state = {
-    title: title,
-    time:time
-  };
+  let title = ctx.request.body.title || ''
+  let time = ctx.request.body.time || ''
+  let tags = ctx.request.body.tags || ''
+  let categories = ctx.request.body.categories || ''
+  let cover = ctx.request.body.cover || ''
+  let verify = ctx.request.body.verify || ''
+  let article = ctx.request.body.article || ''
 
- await ctx.render('create', {});
-});
+  if (verify != '494460') {
+    ctx.body = "you post data:" + JSON.stringify({
+      title: title,
+    })
 
-module.exports = router;
+    ctx.state = {
+      title: title + ': Are you kidding me?'
+    }
+  } else {
+
+    ctx.body = "you post data:" + JSON.stringify({
+      title: title,
+      time: time,
+      tags: tags,
+      categories: categories,
+      cover: cover,
+      article: article
+    })
+
+    ctx.state = {
+      title: title,
+      time: time,
+      tags: tags,
+      categories: categories,
+      cover: cover,
+      article: article
+    }
+    
+    await createMD(title, time, tags, categories, cover, article)
+
+  }
+
+  await ctx.render('create', {})
+
+
+})
+
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-let router = require('koa-router')();
+let router = require('koa-router')()
 
 let gettime = function () {
   Date.prototype.Format = function (fmt) { 
@@ -10,24 +10,23 @@ let gettime = function () {
       "s+": this.getSeconds(), 
       "q+": Math.floor((this.getMonth() + 3) / 3), 
       "S": this.getMilliseconds() 
-    };
-    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length))
     for (var k in o)
-      if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-    return fmt;
+      if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)))
+    return fmt
   }
-  return new Date().Format("yyyy-MM-dd hh:mm:ss");
+  return new Date().Format("yyyy-MM-dd hh:mm:ss")
 }
 
 router.get('/', async function (ctx, next) {
   ctx.state = {
-    title: 'ANR MD Creator',
-    hello: 'vvvvv',
+    title: 'anr md',
     time: gettime()
-  };
+  }
   await ctx.render('index', {
-  });
+  })
 
 
 })
-module.exports = router;
+module.exports = router
